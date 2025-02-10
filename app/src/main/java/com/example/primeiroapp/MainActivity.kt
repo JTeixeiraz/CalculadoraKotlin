b4f844bd71500de1a68b27fbb2c5f5488ddf26a7
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-02-10 18:56:33 -0300

    Primeiro commit com as alterações visuais do projeto, sem lógica envolvida

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.primeiroapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnBotao: Button = findViewById(R.id.btnBotao)
+        val Num1: TextInputLayout = findViewById(R.id.txtNum1)
+        val Num2: TextInputLayout = findViewById(R.id.txtNum2)
+
+        btnBotao.setOnClickListener{
+
+        }
+
+    }
+}
