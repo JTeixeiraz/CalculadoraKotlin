c0e0b9a0036ca70f6d78fefb517ecf1a685127b7
JTeixeiraz <joaopedroteixeirareis@gmail.com>
2025-02-11 16:15:28 -0300

    Logica de soma feita, a calculadora consegue fazer um calculo de soma simples

--- app/src/main/java/com/example/primeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.primeiroapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,10 +23,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         val btnBotao: Button = findViewById(R.id.btnBotao)
-        val Num1: TextInputLayout = findViewById(R.id.txtNum1)
-        val Num2: TextInputLayout = findViewById(R.id.txtNum2)
+        val Num1: TextInputEditText = findViewById(R.id.txtNum1) // Alterado para TextInputEditText
+        val Num2: TextInputEditText = findViewById(R.id.txtNum2) // Alterado para TextInputEditText
+        val Resultado: TextView = findViewById(R.id.txtResultado)
 
-        btnBotao.setOnClickListener{
+        btnBotao.setOnClickListener {
+            val ValorNumero1 = Num1.text?.toString()?.toDoubleOrNull() ?: 0.0 // Removido .editText
+            val ValorNumero2 = Num2.text?.toString()?.toDoubleOrNull() ?: 0.0 // Removido .editText
+            val Operacao = ValorNumero1 + ValorNumero2
+            Resultado.text = "Resultado: ${Operacao}"
 
         }
 
